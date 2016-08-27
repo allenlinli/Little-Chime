9b2ab10be8d76ea15fdf9cb343838d9fadcf6d5b
Li Lin <allenlinli@gmail.com>
2016-08-27 17:56:31 +1000

    Setup the Firebase setting.

---
+++ Pods/Target Support Files/Pods-Little Chime/Pods-Little Chime-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Little_ChimeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Little_ChimeVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Little ChimeTests/Pods-Little ChimeTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Little_ChimeTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Little_ChimeTestsVersionString[];
+
