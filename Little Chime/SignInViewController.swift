4cf1c826bccc028931ef3bfaecc7cb59039ce554
Li Lin <allenlinli@gmail.com>
2016-08-30 17:45:45 +1000

    Having problem accessing the frameworks

--- Little Chime/SignInViewController.swift
+++ Little Chime/SignInViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import FirebaseAuth
+import GoogleSignIn
+import Firebase
 
 class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func signInWithEmail(_ sender: AnyObject) {
+    }
+    
+    @IBAction func signInWithGoogle(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().signIn()
+    }
+    @IBAction func signInWithFacebook(_ sender: AnyObject) {
+        let loginManager = FBSDKLoginManager()
+        loginManager.logInWithReadPermissions(["email"], fromViewController: self, handler: { (result, error) in
+            if let error = error {
+                self.showMessagePrompt(error.localizedDescription)
+            } else if(result.isCancelled) {
+                print("FBLogin cancelled")
+            } else {
+                // [START headless_facebook_auth]
+                let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
+                // [END headless_facebook_auth]
+                self.firebaseLogin(credential)
+            }
+        })
     }
 }
